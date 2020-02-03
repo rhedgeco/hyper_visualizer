8b295ec36b366958f14fb0a1bae20589c0dd7242
rhedgeco <rhedgeco@gmail.com>
2020-02-03 11:58:19 -0800

    Added HyperCore base items

---
+++ Assets/Scripts/HyperCore/HyperCore.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace HyperCore
+{
+    public class HyperCore
+    {
+        public HyperEvent BeginFrame { get; } = new HyperEvent();
+
+        public HyperEvent UpdateFrame { get; } = new HyperEvent();
+
+        public HyperEvent EndFrame { get; } = new HyperEvent();
+
+        public HyperCore()
+        {
+            UpdateFrame.AddListener(DebugHyperEvents);
+        }
+
+        private void DebugHyperEvents(HyperValues values)
+        {
+            Debug.Log($"Frame Time : {values.Time}" +
+                      $"Frame Amplitude : {values.Amplitude}\n" +
+                      $"Frame SpectrumCount : {values.Spectrum.Length}\n" +
+                      $"Frame Hyper Value : {values.HyperValue}");
+        }
+    }
+}
---
+++ Assets/Scripts/HyperCore/HyperEvent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.Events;
+
+namespace HyperCore
+{
+    public class HyperEvent : UnityEvent<HyperValues>
+    {
+        // do nothing
+    }
+
+    public class HyperValues
+    {
+        public float Time { get; }
+        
+        public float Amplitude { get; }
+
+        public float[] Spectrum { get; }
+
+        public float HyperValue { get; }
+
+        public HyperValues(float time, float amplitude, float[] spectrum, float hyperValue)
+        {
+            Time = time;
+            Amplitude = amplitude;
+            Spectrum = spectrum;
+            HyperValue = hyperValue;
+        }
+    }
+}
