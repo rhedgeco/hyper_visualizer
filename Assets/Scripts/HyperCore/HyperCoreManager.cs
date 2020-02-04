56a34c27057cac9f82719f864ff7794df1e86ec8
rhedgeco <rhedgeco@gmail.com>
2020-02-04 14:41:17 -0800

    Added rendering backend start

--- Assets/Scripts/HyperCore/HyperCoreManager.cs
+++ Assets/Scripts/HyperCore/HyperCoreManager.cs
@@ -1,18 +1,81 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using NatCorder;
+using NatCorder.Clocks;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace HyperCore
 {
     public class HyperCoreManager : MonoBehaviour
     {
-        public static HyperCore Core { get; } = new HyperCore();
+        [SerializeField] private MainRenderer _mainRenderer;
+        [SerializeField] private int fps = 60;
+        [SerializeField] private AudioClip _testAudio; //TODO: Replace with actual audio import
 
+        private static HyperCore Core { get; } = new HyperCore();
         public static long CurrentFrame { get; private set; }
+        
+        public static float DeltaTime { get; private set; }
 
         public static void PushFrame(long frameNumber)
         {
             if (frameNumber == CurrentFrame) return;
-            Core.UpdateFrame.Invoke(new HyperValues(0f,0f,new float[2],1f));
+            Core.UpdateFrame.Invoke(new HyperValues(0f, 0f, new float[2], 1f));
             CurrentFrame = frameNumber;
+        }
+
+        public static void ConnectFrameUpdate(UnityAction<HyperValues> method)
+        {
+            Core.UpdateFrame.AddListener(method);
+        }
+
+        private void UpdateFrame()
+        {
+            DeltaTime = 0f; //TODO: Adjust delta
+            Core.UpdateFrame.Invoke(new HyperValues(0f, 0f, new float[2], 1f));
+        }
+
+        public void RenderFootage(float length)
+        {
+            StartCoroutine(RenderRoutine(length));
+        }
+
+        private IEnumerator RenderRoutine(float length)
+        {
+            int audioSamples = _testAudio.frequency;
+            int channels = _testAudio.channels;
+            float[] samples = new float[_testAudio.samples * channels];
+            _testAudio.GetData(samples, 0);
+            int samplesPerFrame = audioSamples / fps;
+
+            MP4Recorder recorder = new MP4Recorder(_mainRenderer.Width, _mainRenderer.Height, fps, audioSamples,
+                channels, s => { Debug.Log(s); });
+            FixedIntervalClock clock = new FixedIntervalClock(fps, false);
+
+            for (int frame = 0; frame < length * fps; frame++)
+            {
+                yield return new WaitForEndOfFrame();
+                long timestamp = clock.Timestamp;
+                Texture2D fTex = _mainRenderer.GetFrame(true);
+                float[] commitSamples = GetPartialSampleArray(samples, samplesPerFrame * frame, samplesPerFrame);
+                recorder.CommitFrame(fTex.GetPixels32(), timestamp);
+                recorder.CommitSamples(commitSamples, timestamp);
+                DestroyImmediate(fTex);
+                UpdateFrame();
+                clock.Tick();
+                Debug.Log($"Generated Frame {frame}/{(int) (length * fps) - 1}");
+            }
+
+            recorder.Dispose();
+        }
+
+        // Copies a smaller section of the sample array
+        private float[] GetPartialSampleArray(float[] samples, int startIndex, int length)
+        {
+            float[] partial = new float[length];
+            Array.Copy(samples, startIndex, partial, 0, length);
+            return partial;
         }
     }
 }
--- Assets/Scripts/HyperCore/MainRenderer.cs
+++ Assets/Scripts/HyperCore/MainRenderer.cs
@@ -1,12 +1,51 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace HyperCore
 {
+    [RequireComponent(typeof(Camera))]
     public class MainRenderer : MonoBehaviour
     {
-        private void Update()
+        private Camera _mainCamera;
+        
+        [SerializeField] private RawImage _imageDisplay;
+
+        public int Width => _mainCamera.targetTexture.width;
+        public int Height => _mainCamera.targetTexture.height;
+
+        private void Awake()
         {
-            HyperCoreManager.PushFrame(1);
+            _mainCamera = GetComponent<Camera>();
+            _mainCamera.targetTexture = new RenderTexture(1920,1080,24,RenderTextureFormat.Default, RenderTextureReadWrite.Linear);
+            _imageDisplay.texture = _mainCamera.targetTexture;
+        }
+
+        private void Start()
+        {
+            RenderFrame();
+        }
+
+        public void RenderFrame()
+        {
+            _mainCamera.Render();
+        }
+
+        public Texture2D GetFrame(bool forceRender = false)
+        {
+            if(forceRender) RenderFrame();
+            return RenderTextureToTexture2D(_mainCamera.targetTexture);
+        }
+
+        // Converts a RenderTexture to Texture2D
+        private Texture2D RenderTextureToTexture2D(RenderTexture rendTex)
+        {
+            Texture2D tex = new Texture2D(rendTex.width, rendTex.height, TextureFormat.RGBA32, false);
+            RenderTexture.active = rendTex;
+            tex.ReadPixels(new Rect(0, 0, rendTex.width, rendTex.height), 0, 0);
+            tex.Apply();
+            return tex;
         }
     }
 }
