579dffc21600f822f42df6c9d3a570f3a09a7327
rhedgeco <rhedgeco@gmail.com>
2020-02-05 11:35:13 -0800

    Added status controller

---
+++ Assets/Scripts/UI/StatusController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Text))]
+    public class StatusController : MonoBehaviour
+    {
+        private static string text;
+        private Text textObject;
+
+        private void Awake()
+        {
+            textObject = GetComponent<Text>();
+        }
+
+        private void Update()
+        {
+            textObject.text = text;
+        }
+
+        public static void UpdateStatus(string status)
+        {
+            text = status;
+        }
+    }
+}
