3fe7d7bf59f19a9c0bb8702c79a1daf5728c29c4
rhedgeco <rhedgeco@gmail.com>
2020-02-05 17:01:03 -0800

    Toolbar title now links to the hypervis website

---
+++ Assets/Scripts/UI/ToolbarTitleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Button))]
+    public class ToolbarTitleController : MonoBehaviour
+    {
+        private Button btn;
+
+        [SerializeField] private string url;
+
+        private void Awake()
+        {
+            btn = GetComponent<Button>();
+            btn.onClick.AddListener(delegate
+            {
+                Application.OpenURL(url);
+            });
+        }
+    }
+}
