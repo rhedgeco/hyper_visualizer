384a7ba94d146a4b156dfeb596207b2d8e2b8d77
rhedgeco <rhedgeco@gmail.com>
2020-02-07 02:24:53 -0800

    Fixed exception with accessing invalid index in sample array

--- Assets/Plugins/Free/FFT/Windowing.cs
+++ Assets/Plugins/Free/FFT/Windowing.cs
@@ -10,8 +10,13 @@ namespace Plugins.Free.FFT
             double[] spec = new double[length];
             for (int i = 0; i < length; i++)
             {
-                double x = samples[(startIndex * 2) + (i * 2) + channel];
-                x *= (-Math.Cos(i * (2d * Math.PI) / length) + 1d) / 2d;
+                int index = (startIndex * 2) + (i * 2) + channel;
+                double x = 0d;
+                if (index < samples.Length - 1)
+                {
+                    x = samples[index];
+                    x *= (-Math.Cos(i * (2d * Math.PI) / length) + 1d) / 2d;
+                }
                 spec[i] = x;
             }
 
