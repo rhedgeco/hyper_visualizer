b7fda84a3385f189801863ebe7c388fb35070ed8
rhedgeco <rhedgeco@gmail.com>
2020-02-07 20:13:07 -0800

    Added input field int limiter

---
+++ Assets/Scripts/UI/InputFieldIntLimiter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(InputField))]
+    public class InputFieldIntLimiter : MonoBehaviour
+    {
+        private InputField field;
+
+        [SerializeField] private int minValue;
+        [SerializeField] private int maxValue;
+
+        private void Awake()
+        {
+            field = GetComponent<InputField>();
+            field.onValueChanged.AddListener(ValueChanged);
+            field.onEndEdit.AddListener(EndEdit);
+        }
+
+        private void ValueChanged(string text)
+        {
+            // do nothing
+        }
+
+        private void EndEdit(string text)
+        {
+            int value;
+            if (!int.TryParse(text, out value))
+            {
+                field.text = "" + minValue;
+                return;
+            }
+
+            if (value > maxValue) value = maxValue;
+            if (value < minValue) value = minValue;
+
+            field.text = "" + value;
+        }
+    }
+}
