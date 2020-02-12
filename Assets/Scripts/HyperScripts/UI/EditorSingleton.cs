d92b56e6a64830794b709011b63a3af2588ca417
rhedgeco <rhedgeco@gmail.com>
2020-02-12 14:47:25 -0800

    added editor singleton

---
+++ Assets/Scripts/HyperScripts/UI/EditorSingleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace HyperScripts.UI
+{
+    public class EditorSingleton : MonoBehaviour
+    {
+        private static EditorSingleton _instance;
+
+        private void Awake()
+        {
+            if (_instance == null) _instance = this;
+            if (_instance != this) Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
